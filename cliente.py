3f1ee0a9617b8bae6a3ddbaf1320b63439a36712
ismaelithalo <ismaelithalo@gmail.com>
2020-11-02 00:28:59 -0300

    Atualizando comentarios

--- cliente.py
+++ cliente.py
@@ -3,28 +3,27 @@
 import socket
 
 CLIENTE = socket.socket(
-        socket.AF_INET,                         # socket.AF_INET - Familia IPV4
-        socket.SOCK_STREAM                      # socket.SOCK_STREAM - Protocolo FTP
+        socket.AF_INET,                                         # socket.AF_INET - Familia IPV4
+        socket.SOCK_STREAM                                      # socket.SOCK_STREAM - Protocolo TCP
         ) 
                                  
 print('Por favor, digite o protocolo, ip destino e porta separados por espaço. (Protocolos disponiveis: HTTP, FTP, SMPT ou IMAP)')
-entrada = input()
-PROTOCOLO, HOST_SERVIDOR, porta_servidor = entrada.split(' ')
-PORTA_SERVIDOR = int(porta_servidor)            # Converte o valor lido de string para int
+entrada = input()                                               # Le os valores
+PROTOCOLO, HOST_SERVIDOR, porta_servidor = entrada.split(' ')   # Pega os valores lidos e separa em variaveis a partir dos espaços 
+PORTA_SERVIDOR = int(porta_servidor)                            # Converte o valor lido de string para int
  
-CLIENTE.connect((
-            HOST_SERVIDOR, 
-            PORTA_SERVIDOR
+CLIENTE.connect((                                               # Solicita conexao com o servidor
+            HOST_SERVIDOR,                                      # IP do servidor a se conectar
+            PORTA_SERVIDOR                                      # Porta a se conectar
         ))
 
-# Envia a mensagem codificada em bytes
-CLIENTE.sendall(PROTOCOLO.encode())             
+
+CLIENTE.sendall(PROTOCOLO.encode())                             # Envia a mensagem codificada em bytes            
 
 print('Conectado com ', HOST_SERVIDOR, PORTA_SERVIDOR)
     
 while True:
-    # Recv ou receive, recebe o dado vindo da conexao com o cliente, 2048 é o buffer recomendado
-    resposta = CLIENTE.recv(2048)
+    resposta = CLIENTE.recv(2048)                               # Recv ou receive, recebe o dado vindo da conexao com o cliente, 2048 é o buffer recomendado
     print('Resposta: ', resposta.decode())
     break
     
