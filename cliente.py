15a9256dfaf159b6fe0ca74593ea523dc5f4349d
ismaelithalo <ismaelithalo@gmail.com>
2020-11-03 23:40:04 -0300

    HTTP e IMAP finalizados

--- cliente.py
+++ cliente.py
@@ -2,28 +2,54 @@
 
 import socket
 
-CLIENTE = socket.socket(                                        # Cria o socket
-        socket.AF_INET,                                         # socket.AF_INET - Familia IPV4
-        socket.SOCK_STREAM                                      # socket.SOCK_STREAM - Protocolo TCP
+CLIENTE = socket.socket(                                                # Cria o socket
+        socket.AF_INET,                                                 # socket.AF_INET - Familia IPV4
+        socket.SOCK_STREAM                                              # socket.SOCK_STREAM - Protocolo TCP
         ) 
                                  
-print('Por favor, digite o protocolo, ip destino e porta separados por espaço. (Protocolos disponiveis: HTTP, FTP, SMPT ou IMAP)')
-entrada = input()                                               # Le os valores
-PROTOCOLO, HOST_SERVIDOR, porta_servidor = entrada.split(' ')   # Pega os valores lidos e separa em variaveis a partir dos espaços 
-PORTA_SERVIDOR = int(porta_servidor)                            # Converte o valor lido de string para int
- 
-CLIENTE.connect((                                               # Solicita conexao com o servidor
-            HOST_SERVIDOR,                                      # IP do servidor a se conectar
-            PORTA_SERVIDOR                                      # Porta a se conectar
-        ))
-
-
-CLIENTE.sendall(PROTOCOLO.encode())                             # Envia a mensagem codificada em bytes            
-
-print('Conectado com ', HOST_SERVIDOR, PORTA_SERVIDOR)
-    
 while True:
-    resposta = CLIENTE.recv(2048)                               # Recv ou receive, recebe o dado vindo da conexao com o cliente, 2048 é o buffer recomendado
-    print('Resposta: ', resposta.decode())
-    break
+        print('Por favor, digite o protocolo, ip destino e porta separados por espaço, ou "FECHAR" para encerrar a comunicação.')
+        print('(Protocolos disponiveis: HTTP, FTP, SMPT ou IMAP)')
+        entrada = input()                                                       # Le os valores
+        if (entrada):
+                if (entrada == 'FECHAR' or entrada == 'fechar' or entrada == 'Fechar'):
+                        break
+                
+                PROTOCOLO, HOST_SERVIDOR, porta_servidor = entrada.split(' ')   # Pega os valores lidos e separa em variaveis a partir dos espaços 
+                PORTA_SERVIDOR = int(porta_servidor)                            # Converte o valor lido de string para int
+
+                CLIENTE.connect((                                               # Solicita conexao com o servidor
+                HOST_SERVIDOR,                                                  # IP do servidor a se conectar
+                PORTA_SERVIDOR                                                  # Porta a se conectar
+                ))
+                
+                print('Conectado com ', HOST_SERVIDOR, PORTA_SERVIDOR)
+
+                if(PROTOCOLO == "HTTP"):
+                        CLIENTE.sendall(PROTOCOLO.encode())                             # Envia a mensagem codificada em bytes
+                        print('\nDigite o método HTTP, seguido por <rota+arquivo> e a versão do HTTP.')
+                        print('(Ex. GET /index.html HTTP/1.1)')
+                        http = input()
+                        request = http+'\r\nHost: localhost\r\n\r\n'
+                        
+                        CLIENTE.sendall(request.encode())                               # Envia a mensagem codificada em bytes            
+                
+                elif (PROTOCOLO == "IMAP"):
+                        CLIENTE.sendall(PROTOCOLO.encode())
+                        print('\nDigite IMAP <usuario> <senha>')
+                        imap = input()
+                        CLIENTE.sendall(imap.encode())
+                
+                elif (PROTOCOLO == "MAIL" or PROTOCOLO == "SMTP"):
+                        print('\nDigite MAIL <usuario> <senha> <from> <to> <subject> <texto>.')
+                        mail = input()
+                        prot, user, senha, remet, dest, assun, texto = mail.split(' ')
+                        CLIENTE.sendall(imap.encode())
+                        
+                resposta = CLIENTE.recv(2048)                                   # Recv ou receive, recebe o dado vindo da conexao com o cliente, 2048 é o buffer recomendado
+                print('Resposta: \n{}'.format(resposta.decode()))
+                break
+                               
+CLIENTE.close()                                                                 # Finaliza a conexao
+                
     
--- servidor.py
+++ servidor.py
@@ -4,6 +4,8 @@ import socket
 
 HOST_SERVIDOR = '127.0.0.1'                         # IP local
 PORTA_SERVIDOR = 11500                              # Porta livre para uso
+CAMINHO_PASTA = '/home/ismael/fr20201g04'           # Pasta com os arquivos de teste
+
 
 SERVIDOR = socket.socket(                           # Cria o socket
             socket.AF_INET,                         # socket.AF_INET - Familia IPV4
@@ -17,13 +19,57 @@ SERVIDOR.bind((                                     # Bind é influenciado pela 
 
 SERVIDOR.listen()                                   # Libera o servidor para conexoes, algo necessario para o TCP
 
-HOST_CLIENTE, PORTA_CLIENTE = SERVIDOR.accept()     # Espera uma conexao com o cliente
-print('Conectado com ', PORTA_CLIENTE)
-    
 while True:
+    SERVIDOR.listen()                                   # Libera o servidor para conexoes, algo necessario para o TCP
+    HOST_CLIENTE, PORTA_CLIENTE = SERVIDOR.accept()     # Espera uma conexao com o cliente
+    print('Conectado com ', PORTA_CLIENTE)
+    
     protocolo = HOST_CLIENTE.recv(2048)             # Recv ou receive, recebe o dado vindo da conexao com o cliente, 2048 é o buffer recomendado
+    
     if (protocolo):
         PROTOCOLO = protocolo.decode()              # A informaçao vem em bytes, por isso precisa ser decodificada
         print('Protocolo solicitado:', PROTOCOLO)
-        HOST_CLIENTE.sendall(b'200 OK')             #Envia mensagem de resposta
-    
+        
+        if (PROTOCOLO == "HTTP"):                       # Se o protocolo for HTTP
+            request = HOST_CLIENTE.recv(2048).decode()  # Recebe os proximos parametros
+            
+            print("\nRequisiçao recebida.")
+            file = request.split(' ')                   # Separa o caminho do arquivo do resto da requisiçao
+            try:
+                with open(CAMINHO_PASTA+'/html'+file[1]) as html:  # Abre o arquivo
+                    text = html.read()
+                    response = 'HTTP/1.1 200 OK\r\n\r\n'+text               # Monta mensagem de resposta
+                    HOST_CLIENTE.sendall(response.encode())                 #Envia mensagem de resposta codificada
+                    print('Resposta enviada.\n')
+            except FileNotFoundError as error:                              # Verifica se houve erro na hora de abrir 
+                        HOST_CLIENTE.sendall(b'404 Not Found\n')            #Envia mensagem de resposta codificada
+                        print('404 Not Found:')
+                        print(request)
+        
+        elif (PROTOCOLO == "SMTP" or PROTOCOLO == "MAIL"):
+            print("SMTP")
+            
+        elif (PROTOCOLO == "IMAP"):                         # Se o protocolo for HTTP
+            request = HOST_CLIENTE.recv(2048).decode()      # Recebe os proximos parametros
+            prot, user, senha = request.split(' ')          # Separa protocolo, usuario e senha vindo da requisiçao
+            
+            print("\nRequisiçao recebida.")
+            
+            with open(CAMINHO_PASTA+'/usuarios.txt') as file:   # Abre o arquivo de usuarios
+                usuarios = file.read().split('\n')              # Separa os itens do arquivo pelas linhas
+                
+                for x in range(len(usuarios)):                  # For com o numero de linhas do arquivo   
+                    u = usuarios[x].split(';')                  # Separa a linha do arquivo em u[0] usuario e u[1] senha
+                    if (u[0]== user):                           # Verifica se o usuario vindo da requisiçao existe no arquivo
+                        if(u[1]== senha):                       # Verifica se a senha da requisiçao eh a senha do arquivo
+                            with open(CAMINHO_PASTA+'/email/'+user+'.txt') as user_file:    # Se tudo estiver certo, abre o arquivo com o nome do usaurio
+                                response = user_file.read()
+                                HOST_CLIENTE.sendall(response.encode())                     # retorna o conteudo do arquivo
+                                print('Resposta enviada.\n')
+                        else:
+                            HOST_CLIENTE.sendall(b'Senha incorreta\n')                      # Retorna senha incorreta pro cliente
+                            print('Resposta enviada.\n')
+                    
+                HOST_CLIENTE.sendall(b'Usuario nao encontrado\n')                           # Retorna usuario nao encontrado pro cliente
+                print('Resposta enviada.\n')   
+                
